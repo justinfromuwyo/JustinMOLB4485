c3202339a1bbe4d2fa6c8f7d6a62d66a99a07d45
justinfromuwyo <jywkoh@gmail.com>
2019-11-19 14:34:01 -0700

    MOLB4485 Day 4 stuff

---
+++ Koh_Phylo/MySecondRAxML.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH -J RAxML
+#SBATCH -n 1
+#SBATCH -t 30:00
+#SBATCH --mem=20g
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jkoh@uwyo.edu
+
+#SBATCH --ntasks-per-node=8
+#SBATCH --account=inbre-train
+
+#each echo commands is just to make sure that all of your code is running properly 
+echo "starting raxmlHPC run at $(date)."
+
+module load swset gcc raxml
+
+#this next bit iof code is all on one line
+
+raxmlHPC-PTHREADS-AVX -T 8 -f a -m GTRGAMMA -p 618 -N 2 -x 309 -s AllBears.aln -n AllBearsTrees -w /project/inbre-train/inbre019/JustinMOLB4485/Koh_Phylo/AllBearsTrees -o Giant Panda
+
+echo "RAxML has finished at $(date)"
